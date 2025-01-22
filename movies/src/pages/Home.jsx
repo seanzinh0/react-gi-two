81298227c9b5a511ac9af94b5ca31576a222a4d6
spichay <seanpichay1015@gmail.com>
2025-01-22 10:46:53 -0500

    make search more modular

--- movies/src/pages/Home.jsx
+++ movies/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import '../App.css'
 import Search from '../components/Search.jsx'
+
 const Home = () => {
     return (
         <>
-            <Search />
+            <div className='flex flex-col justify-center sm:mt-36 gap-5 min-[320px]:mt-16 lg:mt-48'>
+                <h1 className='flex justify-center text-3xl text-center'>Find Similar Movies Here</h1>
+                <Search/>
+            </div>
         </>
     )
 }
--- movies/src/pages/Results.jsx
+++ movies/src/pages/Results.jsx
@@ -2,6 +2,7 @@ import '../App.css'
 import {useLocation} from 'react-router-dom'
 import MovieCards from '../components/MovieCards.jsx'
 import ResultsTitle from "../components/ResultsTitle.jsx";
+import Search from "../components/Search.jsx";
 
 const Results = () => {
     const location = useLocation();
@@ -9,6 +10,9 @@ const Results = () => {
     return(
         <>
             <ResultsTitle props={query}/>
+            <div className='mb-20'>
+            <Search/>
+            </div>
             <div className='flex justify-center'>
             <MovieCards props={results}/>
             </div>
